25526c43eb9c4ded82f40ae5efdcc3d67fcbd128
DwiMunir <Qnorf768@gmail.com>
2022-06-11 09:49:16 +0700

    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +17,51 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
+const getWord = (message) => {
+  const word = message.split(" ")
+  return word?.length >= 1 && word[1]
+}
+
+const getData = (dataset) => {
+  let stringWord = ''
+  if(dataset?.message) stringWord = dataset.message
+
+  if (dataset.length) {
+    dataset?.map((item) => {
+      if(item?.message !== undefined) stringWord = item.message
+      if(item?.data?.message !== undefined) stringWord = item.data.message
+    })
+  }
+
+  return getWord(stringWord)
+}
+
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (callback) => {
+  const list = [file1, file2, file3]
+  const result = []
+
+  list.map((item) => {
+    const dataProcess = new Promise((resolve, reject) => {
+      fs.readFile(item,"utf8", (err, data) => {
+        if (err) {
+          reject(err)
+          return
+        }
+        const parseData = JSON.parse(data)
+        resolve(getData(parseData))
+      })
+    }) 
+    result.push(dataProcess)
+  })
+
+  Promise.all(result).then(values => {
+    callback(null, values)
+  }).catch(error => {
+    callback(error, null)
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
